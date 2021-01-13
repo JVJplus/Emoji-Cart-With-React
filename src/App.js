debfb9f23d166ab12fb63c7a1c7fba3ee90a4208
Jay Prakash Mahto <jvjplus@gmail.com>
2021-01-13 06:45:37 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import CartItem from './CartItem';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <CartItem />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
